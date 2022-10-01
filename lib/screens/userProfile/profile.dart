4c5b54c8e69607e5cf0f5cd366d33b61dd899470
hieutrung0510 <hieutrung0510@gmail.com>
2022-10-02 00:39:34 +0700

    Main Profile Page Done

--- lib/screens/userProfile/profile.dart
+++ lib/screens/userProfile/profile.dart
@@ -1,66 +1,344 @@
+import 'package:edu2gether_mobile/utilities/size_utils.dart';
 import 'package:edu2gether_mobile/widgets/big_text.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class Profile extends StatelessWidget {
+
+class Profile extends StatefulWidget {
 
   const Profile({super.key});
 
   @override
+  State<Profile> createState() => _profileState();
+}
+
+class _profileState extends State<Profile>{
+  @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          leading: BackButton(
-            color: Colors.black,
-            onPressed: (){
-              Navigator.pop(context);
-            },
-          ),
-          title: Text(
-            'Profile',
-            style: TextStyle(
-              fontFamily: 'Urbanist',
-              color: Colors.black,
+    bool _switchValue = true;
 
-            ),
-          ),
-          backgroundColor: Colors.white,
-          elevation: 0,
-          actions: <Widget>[
-            IconButton(onPressed: (){
-                //do something in hear
-            }, icon: Icon(
-              Icons.more_horiz,
-              color: Colors.black,
-
+     return MaterialApp(
+       home: Scaffold(
+         backgroundColor: Colors.white,
+         appBar: AppBar(
+           leading: BackButton(
+             color: Colors.black,
+           ),
+           backgroundColor: Colors.white,
+           elevation: 0,
+           titleTextStyle: TextStyle(
+             color: Colors.black,
+             fontFamily: 'Urbanist',
+             fontSize: 20,
+             fontWeight: FontWeight.bold,
+           ) ,
+           title: const Text(
+               'Profile',
+           ),
+          actions: [
+            IconButton(
+                onPressed: (){},
+                icon: Icon(
+                  Icons.more_horiz_rounded,
+                  color: Colors.black,
             ))
           ],
-        ),
-        body: Material(
-          elevation: 1,
-          child:  Stack(
-            children: [
-              Positioned(
-                left: 90,
-                child:  Column(
-                  children: [
-                    Container(
-                      width: 230,
-                      height: 170,
-                      child: Center(child: Image.asset('assets/images/avatarFBdefault.png'),),
+         ),
+         body: Column(
+           children: [
+             Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
+                  Container(
+                    width: 120,
+                    height: 120,
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/avatarFBdefault.png'),
                     ),
-                    new Text('Username'),
-                    new Text('Email'),
-                  ],
+                  )
+                ],
+             ),
+             Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: <Widget> [
+                 Padding(
+                     padding: EdgeInsets.only(top: 24),
+                    child: Text(
+                      'Hieu Trung',
+                      style: TextStyle(fontFamily: 'Urbanist', fontWeight: FontWeight.w700, fontSize: 24),
+                    ),
+                 ),
+               ],
+             ),
+             Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: <Widget> [
+                 Padding(
+                   padding: EdgeInsets.only(top: 8),
+                   child: Text(
+                     'hieutrung0510@gmail.com',
+                     style: TextStyle(fontFamily: 'Urbanist', fontWeight: FontWeight.w600, fontSize: 14),
+                   ),
+                 ),
+               ],
+             ),
+            Padding(
+                padding:EdgeInsets.only(top: 24, left: 24, right: 24),
+                child: Divider(
+                  thickness: 1,
+                  color: Colors.black,
                 ),
+            ),
+             Row(
+               mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget> [
+                  Container(
+                    width: 380,
+                    height: 28,
+                    child: ListTile(
+                      leading: Icon(Icons.person_outlined,color: Colors.black,),
+                      title: Text(
+                        'Edit Profile',
+                        style: TextStyle(
+                          fontFamily: 'Urbanist',
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600
+                        ),
+                      ),
+                      trailing: Icon(Icons.navigate_next, color: Colors.black,)
+                    ),
+                  )
+                ],
               ),
-          ],),
-        )
+             Padding(
+               padding: EdgeInsets.only(top:20),
+               child:  Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: <Widget> [
+                   Container(
+                     width: 380,
+                     height: 28,
+                     child: ListTile(
+                         leading: Icon(Icons.notifications_none_sharp,color: Colors.black,),
+                         title: Text(
+                           'Notification',
+                           style: TextStyle(
+                               fontFamily: 'Urbanist',
+                               fontSize: 18,
+                               fontWeight: FontWeight.w600,
+                           ),
+                         ),
+                         trailing: Icon(Icons.navigate_next, color: Colors.black,)
+                     ),
+                   )
+                 ],
+               ),
+             ),
+             Padding(
+               padding: EdgeInsets.only(top:20),
+               child:  Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: <Widget> [
+                   Container(
+                     width: 380,
+                     height: 28,
+                     child: ListTile(
+                         leading: Icon(Icons.account_balance_wallet_sharp,color: Colors.black,),
+                         title: Text(
+                           'Payment',
+                           style: TextStyle(
+                             fontFamily: 'Urbanist',
+                             fontSize: 18,
+                             fontWeight: FontWeight.w600,
+                           ),
+                         ),
+                         trailing: Icon(Icons.navigate_next, color: Colors.black,)
+                     ),
+                   )
+                 ],
+               ),
+             ),
+             Padding(
+               padding: EdgeInsets.only(top:20),
+               child:  Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: <Widget> [
+                   Container(
+                     width: 380,
+                     height: 28,
+                     child: ListTile(
+                         leading: Icon(Icons.verified_user_sharp,color: Colors.black,),
+                         title: Text(
+                           'Security',
+                           style: TextStyle(
+                             fontFamily: 'Urbanist',
+                             fontSize: 18,
+                             fontWeight: FontWeight.w600,
+                           ),
+                         ),
+                         trailing: Icon(Icons.navigate_next, color: Colors.black,)
+                     ),
+                   )
+                 ],
+               ),
+             ),
+             Padding(
+               padding: EdgeInsets.only(top:20),
+               child:  Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: <Widget> [
+                   Container(
+                     width: 380,
+                     height: 28,
+                     child: ListTile(
+                         leading: Icon(Icons.language_sharp,color: Colors.black,),
+                         title: Text(
+                           'Language',
+                           style: TextStyle(
+                             fontFamily: 'Urbanist',
+                             fontSize: 18,
+                             fontWeight: FontWeight.w600,
+                           ),
+                         ),
+                         trailing: Icon(Icons.navigate_next, color: Colors.black,)
+                     ),
+                   )
+                 ],
+               ),
+             ),
+             Padding(
+               padding: EdgeInsets.only(top:20),
+               child:  Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: <Widget> [
+                   Container(
+                     width: 380,
+                     height: 28,
+                     child: ListTile(
+                         leading: Icon(Icons.visibility_sharp,color: Colors.black,),
+                         title: Text(
+                           'Dark Mode',
+                           style: TextStyle(
+                             fontFamily: 'Urbanist',
+                             fontSize: 18,
+                             fontWeight: FontWeight.w600,
+                           ),
+                         ),
+                         trailing:
+                            CupertinoSwitch(
+                                value: true,
+                                onChanged: (value){
+                                  setState(() {
+                                    _switchValue = false;
+                                  });
+                                })
+                     ),
+                   )
+                 ],
+               ),
+             ),
+             Padding(
+               padding: EdgeInsets.only(top:20),
+               child:  Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: <Widget> [
+                   Container(
+                     width: 380,
+                     height: 28,
+                     child: ListTile(
+                         leading: Icon(Icons.lock_outline_sharp,color: Colors.black,),
+                         title: Text(
+                           'Privacy Policy',
+                           style: TextStyle(
+                             fontFamily: 'Urbanist',
+                             fontSize: 18,
+                             fontWeight: FontWeight.w600,
+                           ),
+                         ),
+                         trailing: Icon(Icons.navigate_next, color: Colors.black,)
+                     ),
+                   )
+                 ],
+               ),
+             ),
+             Padding(
+               padding: EdgeInsets.only(top:20),
+               child:  Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: <Widget> [
+                   Container(
+                     width: 380,
+                     height: 28,
+                     child: ListTile(
+                         leading: Icon(Icons.info_outline_sharp,color: Colors.black,),
+                         title: Text(
+                           'Help Center',
+                           style: TextStyle(
+                             fontFamily: 'Urbanist',
+                             fontSize: 18,
+                             fontWeight: FontWeight.w600,
+                           ),
+                         ),
+                         trailing: Icon(Icons.navigate_next, color: Colors.black,)
+                     ),
+                   )
+                 ],
+               ),
+             ),
+             Padding(
+               padding: EdgeInsets.only(top:20),
+               child:  Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: <Widget> [
+                   Container(
+                     width: 380,
+                     height: 28,
+                     child: ListTile(
+                         leading: Icon(Icons.people_outline_sharp,color: Colors.black,),
+                         title: Text(
+                           'Invite Friends',
+                           style: TextStyle(
+                             fontFamily: 'Urbanist',
+                             fontSize: 18,
+                             fontWeight: FontWeight.w600,
+                           ),
+                         ),
+                         trailing: Icon(Icons.navigate_next, color: Colors.black,)
+                     ),
+                   )
+                 ],
+               ),
+             ),
+             Padding(
+               padding: EdgeInsets.only(top:20),
+               child:  Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: <Widget> [
+                   Container(
+                     width: 380,
+                     height: 28,
+                     child: ListTile(
+                         leading: Icon(Icons.person_outlined,color: Colors.black,),
+                         title: Text(
+                           'Logout',
+                           style: TextStyle(
+                             fontFamily: 'Urbanist',
+                             fontSize: 18,
+                             fontWeight: FontWeight.w600,
+                           ),
+                         ),
+                         trailing: Icon(Icons.navigate_next, color: Colors.black,)
+                     ),
+                   )
+                 ],
+               ),
+             ),
 
-      ),
-    );
+           ],
+
+         ),
+       ),
+     );
   }
 }
