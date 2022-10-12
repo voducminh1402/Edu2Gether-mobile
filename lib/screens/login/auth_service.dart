b8a76ae5ddbc5ce82f45fabe93b7211e1f6ef86f
learning2html <honganhle0a@gmail.com>
2022-10-12 20:40:31 +0700

    [AnhLH] authentication with user account

--- lib/screens/login/auth_service.dart
+++ lib/screens/login/auth_service.dart
@@ -1,10 +1,13 @@
 import 'package:edu2gether_mobile/main.dart';
+import 'package:edu2gether_mobile/routes/routes.dart';
 import 'package:edu2gether_mobile/screens/login/login.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:get/get.dart';
 
 class AuthService{
+  final auth = FirebaseAuth.instance;
   //Determine if the user is authenticated.
   handleAuthState() {
     return StreamBuilder(
@@ -33,8 +36,57 @@ class AuthService{
     // Once signed in, return the UserCredential
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
+
+  signInWithEmailAndPassword(String email, String password) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+      checkUserState();
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+
+  createUserWithEmailAndPassword(String email, String password) async {
+    try {
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      checkUserState();
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
   //Sign out
   signOut() {
     FirebaseAuth.instance.signOut();
+    checkUserState();
+  }
+
+  checkUserState(){
+    FirebaseAuth.instance
+        .authStateChanges()
+        .listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+        Get.to(() => Login());
+      } else {
+        print('User is signed in!');
+        Get.to(() => MyHomePage());
+      }
+    });
   }
 }
