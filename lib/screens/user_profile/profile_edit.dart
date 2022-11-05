0879b3efb0105f8f69a6f80e00120bc8f2eaaf08
learning2html <honganhle0a@gmail.com>
2022-11-05 14:42:28 +0700

    [AnhLH] login user and edit profile, get user login

--- lib/screens/user_profile/profile_edit.dart
+++ lib/screens/user_profile/profile_edit.dart
@@ -1,9 +1,8 @@
-import 'package:edu2gether_mobile/screens/main_page/main_page.dart';
 import 'package:edu2gether_mobile/screens/user_profile/profile.dart';
 import 'package:edu2gether_mobile/services/mentee_service.dart';
 import 'package:edu2gether_mobile/utilities/colors.dart';
+import 'package:edu2gether_mobile/utilities/dimensions.dart';
 
-import 'package:edu2gether_mobile/widgets/button_update.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter/cupertino.dart';
@@ -11,19 +10,17 @@ import 'package:intl_phone_field/intl_phone_field.dart';
 
 import '../../models/mentee.dart';
 
-import '../../routes/routes.dart';
-import 'package:country_list_pick/country_list_pick.dart';
-import '../../widgets/button_login.dart';
+import 'package:edu2gether_mobile/models/authen_response.dart';
+import 'package:get/get.dart';
 
 
 class ProfileEdit extends StatefulWidget{
 
   String id;
+  AuthenResponse user;
 
 
-
-
-  ProfileEdit({required this.id, Key? key}) : super(key: key);
+  ProfileEdit({required this.id, required this.user, Key? key}) : super(key: key);
 
 
   @override
@@ -41,7 +38,7 @@ class _profileEditState extends State<ProfileEdit> {
   late String _phone;
   late String _address;
   late String _university;
-  late String _country;
+  late String _country = "VN";
   late String _gender;
   late String _image;
 
@@ -53,7 +50,15 @@ class _profileEditState extends State<ProfileEdit> {
 
 
   _getMentee() async{
-    _mentee = (await MenteeService().getMenteeById(widget.id));
+    if(widget.user!.isConfirmedInfo){
+      _mentee = (await MenteeService().getMenteeById(widget.id));
+    }
+    else {
+      isLoaded = true;
+      _fullName = widget.user!.name;
+      _image = widget.user!.image ?? "";
+      _phone = widget.user!.phone ?? "";
+    }
     Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
     if(_mentee != null){
       _country = _mentee!.country;
@@ -62,29 +67,31 @@ class _profileEditState extends State<ProfileEdit> {
       _phone = _mentee!.phone;
       _university = _mentee!.university;
       _image = _mentee!.image;
+      dropdownValue = _mentee!.gender;
       isLoaded = true;
     }
   }
-
-
 
   String dropdownValue = 'Male';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:GestureDetector(
+    return SingleChildScrollView(
+      physics: ScrollPhysics(),
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          minHeight: MediaQuery.of(context).size.height,
+          maxHeight: MediaQuery.of(context).size.height + 300
+        ),
         child: Scaffold(
-          resizeToAvoidBottomInset: false,
-
+          //resizeToAvoidBottomInset: false,
           backgroundColor: Colors.white,
           appBar: AppBar(
             leading: BackButton(
               color: Colors.black,
-              onPressed: () async{
-                Navigator.pop(context,false);
+              onPressed: (){
                 Navigator.pop(context,
-                    MaterialPageRoute(builder: (context) => Profile(id: '')));
+                    MaterialPageRoute(builder: (context) => Profile()));
               },
             ),
             backgroundColor: Colors.white,
@@ -130,7 +137,7 @@ class _profileEditState extends State<ProfileEdit> {
                                   borderSide: BorderSide(color: AppColors.mainColor),
                                   borderRadius: BorderRadius.circular(12)),
                               labelText: 'Full Name',
-                              hintText: _mentee?.fullName,
+                              hintText: _mentee?.fullName ?? widget.user!.name,
                               fillColor: AppColors.inputColor,
                               filled: true,
                             ),
@@ -247,12 +254,12 @@ class _profileEditState extends State<ProfileEdit> {
                         child: IntlPhoneField(
                           decoration: InputDecoration(
                             labelText: 'Phone',
-                            hintText: _mentee?.phone,
+                            hintText: _mentee?.phone ?? widget.user?.phone,
                             border: const OutlineInputBorder(
                               borderSide: BorderSide(),
                             ),
                           ),
-                          initialCountryCode: 'AL',
+                          initialCountryCode: 'VN',
                           onChanged: (phone) {
                             _phone = phone.completeNumber.toString().trim();
                           },
@@ -350,42 +357,40 @@ class _profileEditState extends State<ProfileEdit> {
                   ),
                 ],
               ),
-              Expanded(
-                  child: Align(
-                    alignment: FractionalOffset.bottomCenter,
-                    child: Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: SizedBox(
-                        height: 58,
-                        width: 380,
-                        child:  Card(
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.blue,
-                                minimumSize: Size(MediaQuery.of(context).size.width, 54),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30),
+              SizedBox(
+                height: Dimension.height50,
+              ),
+              SizedBox(
+                            height: 58,
+                            width: 380,
+                            child:  Card(
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.blue,
+                                    minimumSize: Size(MediaQuery.of(context).size.width, 54),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                    textStyle:
+                                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                                onPressed: () {
+                                  Mentee mentee = Mentee(id: widget.id, fullName: _fullName, phone: _phone, address: _address, university: _university, country: _country, gender: _gender, image: _image);
+                                  MenteeService().editMentee(mentee, !widget.user!.isConfirmedInfo);
+                                },
+                                child: Text(
+                                    'Update'
                                 ),
-                                textStyle:
-                                TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                            onPressed: () {
-                              MenteeService().updateMentee(_getMentee());
-                            },
-                            child: Text(
-                                'Update'
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                    ),
-                  )
-              ),
             ],
           ),
 
         ),
       ),
     );
+
+
   }
 
 }
